e5fc6825b3961aa3a963abf1cef34cf0ddb49875
Saket Pareek <saket.pareek90@gmail.com>
2018-09-17 21:48:12 +0530

    Add model data to new model.js file

---
+++ js/model.js
@@ -0,0 +1,22 @@
+let locationsArray = [{
+  name: 'Agrasen Ki Baoli',
+  center: {lat: 28.626049, lng: 77.225038}
+},{
+  name: 'Hanuman Temple',
+  center: {lat: 28.63019, lng: 77.213621}
+},{
+  name: 'Gurudwara Bangla Sahib',
+  center: {lat: 28.626318, lng: 77.209186}
+},{
+  name: 'Jantar Mantar',
+  center: {lat: 28.627055, lng: 77.216627}
+},{
+  name: 'Central Park',
+  center: {lat: 28.632948, lng: 77.219465}
+},{
+  name: 'Janpath',
+  center: {lat: 28.627817, lng: 77.218959}
+},{
+  name: 'Palika Bazar',
+  center: {lat: 28.631009, lng: 77.2186}
+}];
