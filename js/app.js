94671f20e4580ebdc7110275500f183a64e1b268
Saket Pareek <saket.pareek90@gmail.com>
2018-09-18 17:50:46 +0530

    Change syntax position and use of es6

--- js/app.js
+++ js/app.js
@@ -7,19 +7,19 @@ $(function(){
 
     let self = this;
     this.locationList = ko.observableArray([]);
-
-  // Function togglePanel to hide the slide-panel from screen
-    this.togglePanel = function(){
-      $('body').toggleClass('panel-hidden');
-    }
+    let currMarker = self.locationList()[0];
 
     markers.forEach(function(loc){
       console.log(loc.title);
       self.locationList.push(loc);
     });
-    var currMarker = self.locationList()[0];
 
-    self.highlightMarker = function(e){
+    // Function togglePanel to hide the slide-panel from screen
+    this.togglePanel = function(){
+      $('body').toggleClass('panel-hidden');
+    }
+
+    this.highlightMarker = function(e){
       e.setAnimation(google.maps.Animation.BOUNCE);
     }
   }
